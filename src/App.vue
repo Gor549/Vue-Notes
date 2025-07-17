5048b66bdfdafc8b98fe00bb722484974f6a2adc
Gor549 <gor.mkhitaryan4@tumo.org>
2025-07-17 19:33:00 +0400

    HELLO

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import {ref} from 'vue';
+ import Card from'./components/Card.vue'
+let number = ref(7)
+const increment = () =>{
+  number.value++
+}
+</script>
+
+<template>
+  <div>
+    <Card :number="number"/>
+    <button @click="increment">Click me</button>
+  </div>
+
+</template>
+
+<style scoped>
+h1 {
+  color: aqua;
+}
+
+h2 {
+  color: brown;
+}
+</style>
---
+++ src/components/Card.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1> HELLO EWERYBODY
+    </h1>
+    <h2>{{props.number}}</h2>
+</template>
+
+
+<script setup >
+import {defineProps} from 'vue';
+ let props=defineProps(['number'])
+</script>
+
+<style>
+
+
+</style>
